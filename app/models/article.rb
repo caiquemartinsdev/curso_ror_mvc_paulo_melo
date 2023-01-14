0469abfdf7ec6f578418afd2857031aa2dfbaf50
Caique Martins <caiquemartins.dev@gmail.com>
2023-01-14 18:37:32 -0300

    final da aula 6

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 5}
+#este código é para validar o título, e sua presença é obrigatória e o tamanho mínimo e 5
+    validates :body, presence: true
 end
